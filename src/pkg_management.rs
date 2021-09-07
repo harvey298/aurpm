25912beb62c7083ab485b68d4bda132455b1411c
harvey298 <marco77@live.co.uk>
2021-09-07 13:41:36 +0100

    updated pkg management
    remove the use of String later in the cpde and removed useless comments

--- src/pkg_management.rs
+++ src/pkg_management.rs
@@ -9,20 +9,18 @@ pub mod pkgmanagement {
         match env::var(key) {
             Ok(val) => {
                 let working_dir_tmp: String = val + &"/.aurpm/work/".to_string();
-                let working_dir = Path::new(&working_dir_tmp[..]);
-                //println!("changing working directory to {}!", working_dir.display());
+                let working_dir_str: &str = &working_dir_tmp[..];
+                let working_dir = Path::new(working_dir_str);
                 if working_dir.exists() == false {
-                    Command::new("mkdir").arg("-p").arg(&working_dir_tmp[..]).spawn().expect("Failed to run command!").wait().unwrap();
+                    Command::new("mkdir").arg("-p").arg(working_dir_str).spawn().expect("Failed to run command!").wait().unwrap();
                 }
-                //assert!(env::set_current_dir(&working_dir).is_ok());
-                //println!("Successfully changed working directory to {}!", working_dir.display());
-                let mut git_clone = Command::new("git").current_dir(&working_dir_tmp[..]).arg("clone").arg(pkg_url).spawn().expect("failed to execute process");
+                let mut git_clone = Command::new("git").current_dir(working_dir_str).arg("clone").arg(pkg_url).spawn().expect("failed to execute process");
                 git_clone.wait().unwrap();
                 println!("Building!");
                 let pkg_work_dir = working_dir_tmp + &pkg_name;
                 let mut makepkg_cmd: Child = Command::new("makepkg").current_dir(pkg_work_dir).arg("-sic").spawn().expect("failed to execute process");
                 makepkg_cmd.wait().unwrap();
-                println!("Done! and thank you for using aurpm!");
+                println!("Package installed!");
 
             }, 
             Err(e) => println!("couldn't interpret {}: {}", key, e),
