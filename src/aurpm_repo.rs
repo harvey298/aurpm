7ea067a7f8d0b39ca8326fdd398a0c4438e6ae3d
harvey298 <marco77@live.co.uk>
2021-09-15 10:31:36 +0100

    patched up code
    time to set :)

--- src/aurpm_repo.rs
+++ src/aurpm_repo.rs
@@ -1,28 +1,22 @@
 pub mod repoManage {
   pub fn repoAdd() {
     // Repo Command:
-    // repo-add -n -R $HOME/.aurpm/repo/aurpm.db.tar.xz work/*/*.pkg.tar.zst
-    
+   
     // Todo: Add aurpm config reader
         match env::var(key) {
             Ok(val) => {
-                let working_dir_tmp: String = val + &"/.aurpm/work/".to_string();
+                let working_dir_tmp: String = val + &"/.aurpm/repo/".to_string()+"aurpm.db.tar.xz".to_string();
                 let working_dir_str: &str = &working_dir_tmp[..];
                 let working_dir = Path::new(working_dir_str);
                 if working_dir.exists() == false {
                     Command::new("mkdir").arg("-p").arg(working_dir_str).spawn().expect("Failed to run command!").wait().unwrap();
-                }
-                let mut git_clone = Command::new("git").current_dir(working_dir_str).arg("clone").arg(pkg_url).spawn().expect("failed to execute process");
-                git_clone.wait().unwrap();
-                println!("Building!");
-                let pkg_work_dir = working_dir_tmp + &pkg_name;
-                let mut makepkg_cmd: Child = Command::new("makepkg").current_dir(pkg_work_dir).arg("-sic").spawn().expect("failed to execute process");
-                makepkg_cmd.wait().unwrap();
-                println!("Package installed!");
+                } // Start Repo 
+                  // repo-add -n -R $HOME/.aurpm/repo/aurpm.db.tar.xz work/*/*.pkg.tar.zst
+                  Command::new("repo-add").arg("-n").arg("-R").arg(working_dir_str).arg("work/*/*.pkg.tar.zst").spawn().expect("Failed to run cmd").wait().unwrap();
+                  println!("Finished Repo");
 
             }, 
             Err(e) => println!("couldn't interpret {}: {}", key, e),
         }
-    let _aurpm_repo: &str = ""
   }
 }
