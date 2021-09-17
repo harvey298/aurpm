058c7327ef2b5a7fadfe059563a8377eb107e58e
harvey298 <marco77@live.co.uk>
2021-09-17 21:23:15 +0100

    fixed git mistakes

--- src/pkg_management.rs
+++ src/pkg_management.rs
@@ -9,20 +9,12 @@ pub mod pkgmanagement {
         match env::var(key) {
             Ok(val) => {
                 let working_dir_tmp: String = val + &"/.aurpm/work/".to_string();
-<<<<<<< HEAD
-                let working_dir = Path::new(&working_dir_tmp[..]);
-=======
                 let working_dir_str: &str = &working_dir_tmp[..];
                 let working_dir = Path::new(working_dir_str);
->>>>>>> b191e61ed56587c94c810f6bd9845fd467bb81f8
                 if working_dir.exists() == false {
                     Command::new("mkdir").arg("-p").arg(working_dir_str).spawn().expect("Failed to run command!").wait().unwrap();
                 }
-<<<<<<< HEAD
-                let mut git_clone = Command::new("git").current_dir(&working_dir_tmp[..]).arg("clone").arg(pkg_url).spawn().expect("failed to execute process");
-=======
                 let mut git_clone = Command::new("git").current_dir(working_dir_str).arg("clone").arg(pkg_url).spawn().expect("failed to execute process");
->>>>>>> b191e61ed56587c94c810f6bd9845fd467bb81f8
                 git_clone.wait().unwrap();
                 println!("Building!");
                 let pkg_work_dir = working_dir_tmp + &pkg_name;
